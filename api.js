d28d8bbb56c44c08faa456b5cac895000fcc0dcc
hoangvu1x02 <55611038+hoangvu1x02@users.noreply.github.com>
2019-11-16 18:23:15 +1100

    Add files via upload

---
+++ api.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    $(".searchbykeyword").on("click", function (event) {
+        event.preventDefault();
+        var name = $(".keyword").val().toLowerCase().trim();
+        console.log(name);
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + name + "&apikey=J1LIFHjLvkNEcD4gPnYHGcQNfXstsT5J"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+    });
+
+    $(".submit").on("click", function (event) {
+        event.preventDefault();
+        var country = $(".country").val().toLowerCase().trim();
+        var countryCode;
+        console.log(country);
+        var secondqueryURL = "https://restcountries.eu/rest/v2/name/" + country;
+        $.ajax({
+            url: secondqueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            countryCode = response[0].alpha2Code;
+            console.log(countryCode);
+
+
+
+            var thirdqueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode="+ countryCode +"&startDateTime=2019-11-16T14:00:00Z&endDateTime=2019-12-31T14:00:00Z&city=melbourne&apikey=J1LIFHjLvkNEcD4gPnYHGcQNfXstsT5J"
+            $.ajax({
+                url: thirdqueryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+            });
+
+        });
+
+
+    });
+});
