25ff1f5801f0e50d21f415747195a4499260a577
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-18 21:58:13 +1100

    rename file & var and add map key

--- assets/js/app_settings.js
+++ assets/js/app_settings.js
@@ -1,4 +1,4 @@
-const API_SETTTINGS = {
+const TM_SETTTINGS = {
     baseURL: "https://app.ticketmaster.com",
     image: {
         ratio: "16_9",
@@ -9,3 +9,5 @@ const API_SETTTINGS = {
     
 
 };
+
+const MAP_KEY = 'pk.eyJ1Ijoicm9zeXRpZ2VyIiwiYSI6ImNrMzQ2dmdnMzB6bjQzY21vanMzcmZwN2gifQ.SmS1JTHUQKYr_roaQIAMFQ';
