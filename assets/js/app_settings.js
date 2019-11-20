01a70ec596ae9a29505e0ed4d1c6171c07bf2efb
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-20 20:13:06 +1100

    add storage key

--- assets/js/app_settings.js
+++ assets/js/app_settings.js
@@ -1,4 +1,5 @@
 //TM stands for Ticket Masters
+// settings for Ticketmaster data
 const TM_SETTTINGS = {
     baseURL: "https://app.ticketmaster.com",
     image: {
@@ -9,13 +10,19 @@ const TM_SETTTINGS = {
 
 };
 
+// for Mapbox
 const MAP_KEY = 'pk.eyJ1Ijoicm9zeXRpZ2VyIiwiYSI6ImNrMzQ2dmdnMzB6bjQzY21vanMzcmZwN2gifQ.SmS1JTHUQKYr_roaQIAMFQ';
 
 
+// Messages for the user.
 const MESSAGES = {
     offsales: "Ticket sales are now closed.",
     mapWrongDataType : "Wrong coords data type"
 }
 
+// date time format
 const DATE_FORMAT = "DD-MMM-YYYY";
 const TIME_FORMAT = "hh:mmA";
+
+// for local storage
+const STORAGE_KEY = "favourites";
