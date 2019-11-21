ea84dc80b144aaee58b67f79fe9893f2a106bfe2
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-21 21:31:39 +1100

    add faourite rendering and count down

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -8,15 +8,106 @@ let FAVOURITES = [];
  * @type StorageHandler 
  */
 const STORAGE = new StorageHandler(STORAGE_KEY, FAVOURITES);
+/**
+ * @type intervalID
+ */
+let TIMER_ID;
+
+function populateFavourites() {
+    const container = $(".favouites-container");
+    const elements = [];
+    const today = moment();
+    const length = FAVOURITES.length;
+    let daysLeftToNext = Number.POSITIVE_INFINITY;
+    //clean up the container
+    container.empty();
+    for (let i = 0; i < length; i++) {
+        const favourite = FAVOURITES[i];
+        const favElement = $(".favourite.template").clone().removeClass("template");
+        const favDate = moment(favourite.dates.start);
+
+        favElement.attr("data-id", favourite.id);
+        favElement.attr("data-index", i);
+        favElement.find(".title").text(favourite.name);
+        if (favDate.isSameOrBefore(today)) {
+            favElement.find(".days").text("0");
+            favElement.addClass("past");
+        } else {
+            const daysLeft = favDate.diff(today, "days");
+            if (daysLeft < daysLeftToNext) {
+                const pinned = $(".pinned-container");
+                const hoursLeft = favDate.diff(today, "hours") - (daysLeft * 24);
+                const minutesLeft = favDate.diff(today, "minutes") - (hoursLeft * 60) - (daysLeft * 24 * 60);
+                const secondsLeft = favDate.diff(today, "seconds") - (minutesLeft * 60) - (hoursLeft * 60 * 60) - (daysLeft * 24 * 60 * 60);
+
+                //update pinned section
+                pinned.find(".days").text(daysLeft).data("days", daysLeft);
+                pinned.find(".hours").text(hoursLeft).data("hours", hoursLeft);
+                pinned.find(".minutes").text(minutesLeft).data("minutes", minutesLeft);
+                pinned.find(".seconds").text(secondsLeft).data("seconds", secondsLeft);
+                pinned.find(".event-title").text(favourite.name);
+                daysLeftToNext = daysLeft;
+                //start the timer
+                TIMER_ID = setInterval(() => {
+                    const pinned = $(".pinned-container");
+                    const secondsElement = pinned.find(".seconds");
+                    let seconds = parseInt(secondsElement.data("seconds"));
+                    console.log(seconds)
+                    if (seconds === 0) {
+                        const minutesElement = pinned.find(".minutes");
+                        let minutes = parseInt(minutesElement.data("minutes"));
+
+                        if (minutes === 0) {
+                            const hoursElement = pinned.find(".hours");
+                            let hours = parseInt(hoursElement.data("hours"));
+
+                            if (hours === 0) {
+                                const daysElement = pinned.find(".days");
+                                let days = parseInt(daysElement.data("days"));
+                                //reduce one day and reset hours to 24
+                                days--;
+                                hours = 24;
+                                //update daysElement
+                                daysElement.text(days);
+                                daysElement.data("days", days);
+                            }
+                            //reduce one hours and reset minutes
+                            hours--;
+                            minutes = 60;
+                            //update hours element
+                            hoursElement.text(hours);
+                            hoursElement.data("hours", hours);
+                        }
+                        //reduce one minute and reset seconds
+                        minutes--;
+                        seconds = 60;
+                        //update minutes element
+                        minutesElement.text(minutes);
+                        minutesElement.data("minutes",minutes);
+                    }
+                    //reduce seconds by one
+                    seconds--;
+                    //update second element
+                    secondsElement.text(seconds);
+                    secondsElement.data("seconds", seconds);
+
+                }, 1000);
+                continue;
+            }
+            favElement.find(".days").text(daysLeft);
+        }
+
+        elements.push(favElement);
+    }
+
+    container.append(elements);
 
 
-function populateFavourites(){
-    
 }
 
 $(document).ready(function () {
     FAVOURITES = STORAGE.data;
-    
+
 
     $("#event-details-container").on("click", "button.toggle-favourite", function () {
         const target = $(this);
