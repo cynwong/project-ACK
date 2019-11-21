d241b4ca6f2e319dc96fb6e2eefd7317364d42d9
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-21 18:23:01 +1100

    fix Favourite obj link to current event object

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -2,7 +2,7 @@
 /**
  * @type object[]
  */
-const FAVOURITES = [];
+let FAVOURITES = [];
 
 /**
  * @type StorageHandler 
@@ -10,51 +10,59 @@ const FAVOURITES = [];
 const STORAGE = new StorageHandler(STORAGE_KEY, FAVOURITES);
 
 
-/**
- * Find an event in Favourite list by id
- * @param {number|string} id 
- * @return {object} event in Favourites List. 
- */
-function findFavourite(id) {
-    return FAVOURITES.find((event) => event.id === id);
-}
-
-/**
- *  Add new Favourite
- * @param {object} event 
-//  */
-// function addFavourite(event){
-//     FAVOURITES.push(event);
-// }
-
-/**
- * remove Favourite by id
- * @param {number|string} id
- */
-function removeFavourite(id){
-    const index = FAVOURITES.findIndex(event=>event.id === id);
-
-    FAVOURITES.splice(index,1);
+function populateFavourites(){
     
 }
 
-const PINNED = {};
+$(document).ready(function () {
+    FAVOURITES = STORAGE.data;
+    
 
-$(document).ready(function(){
-    $("#event-details-container").on("click", "button.toggle-favourite", function(){
+    $("#event-details-container").on("click", "button.toggle-favourite", function () {
         const target = $(this);
 
-        if(target.hasClass("saved")){
+        if (target.hasClass("saved")) {
             // delete the current event 
             target.removeClass("saved");
-            const id = arget.closest(".event-details").data("eventid");
-            const index = FAVOURITES.findIndex(event=>event.id === id);
+            const id = target.closest(".event-details").data("eventid");
+            const index = FAVOURITES.findIndex(event => event.id === id);
 
-            FAVOURITES.splice(index,1);
-        }else{
+            FAVOURITES.splice(index, 1);
+        } else {
             //add this event to favourite
             target.addClass("saved");
-            FAVOURITES.push(CURRENT_EVENT);
+            const newFavourite = {
+                attractions: CURRENT_EVENT.attractions,
+                classifications: CURRENT_EVENT.classifications,
+                dates: CURRENT_EVENT.dates,
+                id: CURRENT_EVENT.id,
+                imageUrl: CURRENT_EVENT.imageUrl,
+                info: CURRENT_EVENT.info,
+                name: CURRENT_EVENT.name,
+                pleaseNote: CURRENT_EVENT.pleaseNote,
+                sales: CURRENT_EVENT.sales,
+                url: CURRENT_EVENT.url,
+                venues: CURRENT_EVENT.venues
+            };
+            FAVOURITES.push(newFavourite);
+            FAVOURITES.sort(fav1, fav2 => {
+                const date1 = moment(fav1.dates.start);
+                const date2 = moment(fav2.dates.start);
+
+                if (date1.isBefore(date2) === true) {
+                    return -1;
+                } else if (date1.isSame(date2) === true) {
+                    return 0;
+                } else if (date1.isAfter(date2) === true) {
+                    return 1;
+                }
+            });
         }
+        STORAGE.data = FAVOURITES;
     });
+
+    $(".modal-close").click(function () {
+        $(".modal").removeClass("is-active");
+    })
 });
+
